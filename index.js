19afe1343749046739e792e06837329835f58a60
aymane <aymanefront20042@gmail.com>
2024-01-28 16:45:11 +0100

    heloo owlr

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const connectDB = require( './mongodb/connect.js');
 const Route = require('./routes/route.js');
 const Routesss = require('./routes/send.js');
 
-const  {readdirSync}  = require('fs')
 require('dotenv').config()
 const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
-// app.use('/hello', Route);
-// app.use('/send', Routesss);
+app.use('/hello', Route);
+app.use('/send', Routesss);
 app.get('/', (req, res) => {
     res.send("hello world");
 });
@@ -29,7 +28,7 @@ const startServer = async () => {
     }
 };
 
-// startServer();
+startServer();
 app.listen(PORT, () => console.log("listening on port :" + PORT));
 
 
