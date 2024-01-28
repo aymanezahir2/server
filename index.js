f8900a160519f30424abd45359eceeb7d852bb5e
aymane <aymanefront20042@gmail.com>
2024-01-28 16:35:38 +0100

    heloo owlr

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 // app.use('/hello', Route);
 // app.use('/send', Routesss);
-readdirSync("./routes").map((file)=>app.use("/",require("./routes/"+file)))
 app.get('/', (req, res) => {
     res.send("hello world");
 });
@@ -32,3 +31,6 @@ const startServer = async () => {
 
 // startServer();
 app.listen(PORT, () => console.log("listening on port :" + PORT));
+
+
+module.exports = app;
